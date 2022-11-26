2d6dd28be36313367c733a1d33dc331c721cb05e
Allan Jacobs <ajjacobs01@kellerisd.com>
2022-11-25 18:25:05 -0600

    Improve composition for filters

--- plugin/filters.go
+++ plugin/filters.go
@@ -4,7 +4,29 @@ import (
 	"os/exec"
 )
 
-func HasExec(name string) func() bool {
+type Predicate func() bool
+
+func (p Predicate) Or(predicate Predicate) Predicate {
+	return func() bool {
+		return p() || predicate()
+	}
+}
+
+func (p Predicate) And(predicate Predicate) Predicate {
+	return func() bool {
+		return p() && predicate()
+	}
+}
+
+func True() Predicate {
+	return func() bool { return true }
+}
+
+func False() Predicate {
+	return func() bool { return false }
+}
+
+func HasExec(name string) Predicate {
 	return func() bool {
 		_, err := exec.LookPath(name)
 		return err == nil
